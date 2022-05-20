178597813fee3bbb85325a97d07394ec91dd61fa
HabibullaSonet <sonet.swe@gmail.com>
2022-05-21 01:00:41 +0600

    Get Value and Split

--- task1.js
+++ task1.js
@@ -1,8 +1,9 @@
-let message = document.getElementById("message").innerHTML;
 
-let messageArray = message.split(". ");
+function splitText(){
+    let message = document.getElementById("input").value;
+    let messageArray = message.split(". ");
 
-for (i=0; i<messageArray.length; i++){
-    document.write(`${messageArray[i]}.<br>`);
-    document.getElementById("message").innerHTML = "";
+    for (i=0; i<messageArray.length; i++){
+        document.write(`${messageArray[i]}.<br>`);
+    }
 }
