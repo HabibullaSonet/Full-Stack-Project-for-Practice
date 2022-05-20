c290861721aad4068019ea6b385d8fca8a727891
HabibullaSonet <sonet.swe@gmail.com>
2022-05-21 00:38:14 +0600

    Full Stop Added

--- task1.js
+++ task1.js
@@ -3,6 +3,6 @@ let message = document.getElementById("message").innerHTML;
 let messageArray = message.split(". ");
 
 for (i=0; i<messageArray.length; i++){
-    document.write(messageArray[i] + "<br >");
+    document.write(`${messageArray[i]}.<br>`);
     document.getElementById("message").innerHTML = "";
 }
